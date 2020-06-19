5cd5b47ab8e3d568d45ac2006b7e4bfae5908df8
Zean Qin <dev@zean.be>
2020-06-19 13:49:16 +1000

    Add simple slot example

---
+++ components/slots-and-scoped-slots/Button.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="btn">{{ name }} - <slot /></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    name: {
+      type: String,
+      default: '',
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+$primary-color: #00ae76;
+
+.btn {
+  border: 2px solid $primary-color;
+  padding: 10px;
+  font-size: 16px;
+  border-radius: 20px;
+  color: $primary-color;
+
+  &:hover {
+    background-color: $primary-color;
+    color: white;
+  }
+}
+</style>
---
+++ pages/slots-and-scoped-slots.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-container>
+    <my-button name="Some other data">Button 1</my-button>
+    <my-button name="Some other data">Button 2</my-button>
+  </b-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Button from '~/components/slots-and-scoped-slots/Button.vue'
+export default Vue.extend({
+  components: {
+    'my-button': Button,
+  },
+})
+</script>
